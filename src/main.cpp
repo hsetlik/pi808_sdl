22d7948b8ecaa379c2830e93d5533c49d70d2876
Hayden Setlik <haydensetlik@gmail.com>
2022-07-01 21:23:56 -0600

    added sdl2

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
+#define DISPLAY_WIDTH 800
+#define DISPLAY_HEIGHT 480
+
 int main()
 {
   if (SDL_Init(SDL_INIT_EVERYTHING) > 0)
@@ -8,11 +11,10 @@ int main()
     std::cout << "SDL_Init failed with error: " << SDL_GetError() << std::endl;
     return EXIT_FAILURE;
   }
+  auto window = SDL_CreateWindow("Pi808", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, DISPLAY_WIDTH, DISPLAY_HEIGHT,0);
 
-  std::string greetings = "Hello SDL2!";
-  std::cout << greetings << std::endl;
 
-  SDL_Quit();
+  while (1) ;
 
   return EXIT_SUCCESS;
 }
